3cdd60a74161666ff4d778af283352dc1331f601
virenabhyankar <forvirenra@gmail.com>
2019-12-01 01:04:54 -0800

    Added functionality for quantity buttons

--- app/public/js/menu.js
+++ app/public/js/menu.js
@@ -1,5 +1,5 @@
 window.onload = function() { 
-    const button = this.document.getElementById('sendMenuData');
+    /*const button = this.document.getElementById('sendMenuData'); /// Example code
     const para = this.document.getElementById('menuInfo');
     
     // Post request to page.
@@ -13,5 +13,41 @@ window.onload = function() {
         });
 
         para.innerText = 'Data sent!';
+    }*/
+    
+    const showBtn = this.document.getElementById('modal-trig');
+    var quantity = 1;
+
+    showBtn.onclick = function() {
+        $('#itemModal').modal('show');
+        quantity = 1;
     }
+
+    const subtract = this.document.getElementById('subtract');
+    const add = this.document.getElementById('add');
+    var span = this.document.getElementById('quantity');
+    var cart = this.document.getElementById('cart-text');
+
+    subtract.onclick = function() {
+        if (quantity > 1) {
+            span.innerHTML = --quantity;
+            cart.innerHTML = "Add "+quantity+" to cart";
+            console.log("subtracted quantity");
+        } else {
+            console.log("tried to subtract from 1");
+        }
+    }
+
+    add.onclick = function() {
+        span.innerHTML = ++quantity;
+        cart.innerHTML = "Add "+quantity+" to cart";
+        console.log("added quantity");
+    }
+
+    $('#itemModal').on('hidden.bs.modal', function () {
+        quantity = 1;
+        span.innerHTML = quantity;
+        cart.innerHTML = "Add "+quantity+" to cart";
+        console.log("cleared quantity")
+    })
 }

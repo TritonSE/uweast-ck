527c942148bfd663720a365f35dd2eead447f291
virenabhyankar <forvirenra@gmail.com>
2019-12-01 01:29:17 -0800

    Cleared quantity after modal is hidden

--- app/public/js/menu.js
+++ app/public/js/menu.js
@@ -15,39 +15,37 @@ window.onload = function() {
         para.innerText = 'Data sent!';
     }*/
     
-    const showBtn = this.document.getElementById('modal-trig');
     var quantity = 1;
-
-    showBtn.onclick = function() {
+    $('#modal-trig').click(function() {
         $('#itemModal').modal('show');
-        quantity = 1;
+        setQuantity(1);
+    });
+
+    function setQuantity(quant) {
+        console.log("quantity is "+quant)
+        $('#quantity').html(quant);
+        $('#cart-text').html("Add "+quant+" to cart");
+        console.log("set quantity to "+quant);
     }
 
-    const subtract = this.document.getElementById('subtract');
-    const add = this.document.getElementById('add');
-    var span = this.document.getElementById('quantity');
-    var cart = this.document.getElementById('cart-text');
-
-    subtract.onclick = function() {
+    $('#subtract').click(function() {
         if (quantity > 1) {
-            span.innerHTML = --quantity;
-            cart.innerHTML = "Add "+quantity+" to cart";
-            console.log("subtracted quantity");
+            setQuantity(--quantity);
         } else {
             console.log("tried to subtract from 1");
         }
-    }
+    });
 
-    add.onclick = function() {
-        span.innerHTML = ++quantity;
-        cart.innerHTML = "Add "+quantity+" to cart";
-        console.log("added quantity");
-    }
+    $('#add').click(function() {
+        setQuantity(++quantity);
+    });
 
     $('#itemModal').on('hidden.bs.modal', function () {
         quantity = 1;
-        span.innerHTML = quantity;
-        cart.innerHTML = "Add "+quantity+" to cart";
-        console.log("cleared quantity")
+
+        $('.form-check-input').prop('checked', false);
+        console.log("cleared radio and checkbox");
+
+        $('#specialInstructionsText').val("");
     })
 }

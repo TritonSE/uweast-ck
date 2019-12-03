e686450767d42b636f9fcf2909c7f6cad1e4e8cb
NirmalAgnihotri <53363382+NirmalAgnihotri@users.noreply.github.com>
2019-12-03 14:13:05 -0800

    Fixed lint errors

--- app/routes/orders/index.js
+++ app/routes/orders/index.js
@@ -1,17 +1,15 @@
 const express = require('express');
-const log = require('../../logger');
 
 const router = express.Router();
 
 // Regular get, no params or extra routing.
 router.get('/', (req, res, next) => {
-    res.render('orders');
+  res.render('orders');
 });
 
 // Post data, log data to terminal.
 router.post('/', (req, res) => {
-    console.log(req.body);
-    res.json({ error: null });
+  res.json({ error: null });
 });
 
 module.exports = router;

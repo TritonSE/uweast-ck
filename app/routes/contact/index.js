ecf78dbef0be848ed09ee3ddcca04b752168d05a
Dhruv Sood <53026542+dhrvsood@users.noreply.github.com>
2019-12-03 14:04:13 -0800

    made adjustments from lint
    removed console.log and unused statements

--- app/routes/contact/index.js
+++ app/routes/contact/index.js
@@ -1,19 +1,15 @@
 const express = require('express');
-const log = require('../../logger');
 
 const router = express.Router();
 
-console.log("contact page");
-
 // Regular get, no params or extra routing.
 router.get('/', (req, res, next) => {
-    res.render('contact');
+  res.render('contact');
 });
 
 // Post data, log data to terminal.
 router.post('/', (req, res) => {
-    console.log(req.body);
-    res.json({ error: null });
+  res.json({ error: null });
 });
 
 module.exports = router;

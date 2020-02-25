ce83b53132917254ebc54458bd52ac4ca1594242
virenabhyankar <forvirenra@gmail.com>
2020-02-24 23:12:11 -0800

    Connect to database using mongoose

--- index.js
+++ index.js
@@ -4,8 +4,23 @@ const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
 const app = require('./app');
 
+const mongoose = require('mongoose');
+const url = "mongodb+srv://virenabhyankar:uweastdb@cluster0-ro3dc.azure.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true});
+
+const db = mongoose.connection;
+
 const log = app.logger;
 const server = express();
+
+// database
+db.once('open', _ => {
+  console.log('Database connected:', url);
+});
+
+db.on('error', err => {
+  console.error('connection error:', err);
+});
 
 // view engine setup
 server.set('views', path.join(__dirname, 'app/views'));

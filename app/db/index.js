0a383f70e2954fce0bcd052af454b675f24aa771
virenabhyankar <forvirenra@gmail.com>
2020-02-29 00:21:18 -0800

    Remove connection variable

--- app/db/index.js
+++ app/db/index.js
@@ -4,14 +4,3 @@ const uri = config.db.uri;
 const { Item } = require('./models/item');
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true});
-
-const db = mongoose.connection;
-
-// database
-db.once('open', _ => {
-    console.log('Database connected:', uri);
-});
-
-db.on('error', err => {
-    console.error('connection error:', err);
-});

7af39597c95f49a97a36f5df40fc986b8a90f484
virenabhyankar <forvirenra@gmail.com>
2020-03-03 13:04:40 -0800

    Query all data

--- app/routes/menu/index.js
+++ app/routes/menu/index.js
@@ -4,7 +4,7 @@ const log = require('../../logger');
 
 const router = express.Router();
 
-// Regular get for all menu items
+// Regular get, no params or extra routing.
 router.get('/', (req, res, next) => {
   const items = [];
   db.getAllMenuItems().then((allItems) => {
@@ -12,10 +12,23 @@ router.get('/', (req, res, next) => {
     
     for (const key in allItems) {
       const childData = allItems[key];
-      console.log("Child data: ", childData);
-      items.push(childData);
+      
+      items.push({
+        name: childData.name,
+        description: childData.description,
+        price: childData.price,
+        category: childData.category,
+        image: childData.image,
+        cuisine: childData.cuisine,
+        tags: childData.tags,
+        vegan: childData.vegan,
+        vegetarian: childData.vegetarian,
+        glutenFree: childData.glutenFree,
+        ingredients: childData.ingredients,
+      })
     }
-    res.render('menu', {items});
+    console.log(items);
+    res.render('items', {items});
   }).catch((error) => {
     log.error(error);
   });

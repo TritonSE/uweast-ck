2708f54ba0b65dd7f90c1df82faa6510cb9cf4af
virenabhyankar <forvirenra@gmail.com>
2020-03-03 18:50:09 -0800

    Lint fix

--- app/db/index.js
+++ app/db/index.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 const config = require('../config');
 const { Item } = require('./models/item');
 
-const uri = config.db.uri;
+const { uri } = config.db;
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 function getAllMenuItems() {
-	return Item.find({}).exec();
+  return Item.find({}).exec();
 }
 
 module.exports = { getAllMenuItems };
--- app/db/models/item.js
+++ app/db/models/item.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 
 const itemSchema = new mongoose.Schema({
-	name: String,
-	description: String,
-	price: Number,
-	category: String,
-	image: String,
-	cuisine: String,
-	tags: [String],
-	vegan: Boolean,
-	vegetarian: Boolean,
-	glutenFree: Boolean,
-	ingredients: [String],
+  name: String,
+  description: String,
+  price: Number,
+  category: String,
+  image: String,
+  cuisine: String,
+  tags: [String],
+  vegan: Boolean,
+  vegetarian: Boolean,
+  glutenFree: Boolean,
+  ingredients: [String],
 });
 const Item = mongoose.model('Item', itemSchema);
 

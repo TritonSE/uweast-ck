2812813562efb50ee4e52a0f360497b7a2980f72
virenabhyankar <forvirenra@gmail.com>
2020-04-19 21:58:40 -0700

    Change item id reference

--- app/db/models/payment.js
+++ app/db/models/payment.js
@@ -1,15 +1,14 @@
 const mongoose = require('mongoose');
-const { Item } = require('item');
 
 const paymentSchema = new mongoose.Schema({
   quantity: Number,
-  items: [Item],    // payment ID, orders database, hash something possible?
-    /*name: String,
+  items: String, // payment ID, orders database, hash something possible?
+  /* name: String,
     size: String,
     special: String,
     */
-  //}, // how to do this to hold Item objects?
-  //size...
+  // }, // how to do this to hold Item objects?
+  // size...
   subtotal: Number,
   tax: Number,
   tip: Number,

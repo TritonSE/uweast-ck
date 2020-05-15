9688ba4175f344142133cc22c74f49e62f9f6e85
ganheather02 <ganheather02@gmail.com>
2020-05-14 18:53:17 -0700

    finished making form + css

--- app/public/js/items.js
+++ app/public/js/items.js
@@ -3,13 +3,20 @@ class Item {
         this.id = id;
         this.name = name;
         this.description = description;
+        this.price = price;
         this.category = category;
+        this.image = image;
+        this.cuisine = cuisine;
+        this.vegan = vegan;
+        this.vegetarian = vegetarian;
+        this.glutenFree = glutenFree
+        this.ingredients = ingredients;
     }
 }
 
 
-$('.random').click(function() {
-    const form = $('.createItem');
+$('.submit-button').click(function() {
+    const form = $('.new-item-form');
     var responses = form.serializeArray();
 
     if (!form[0].checkValidity()) {
@@ -40,14 +47,26 @@ $('.random').click(function() {
         if (responses[key].name === 'image' && responses[key].value !== '') {
             image = responses[key].value;
         }
+        if (responses[key].name === 'cuisine' && responses[key].value !== '') {
+            cuisine = responses[key].value;
+        }
         if (responses[key].name === 'vegan' && responses[key].value !== '') {
             vegan = responses[key].value;
         }
+        if (responses[key].name === 'glutenFree' && responses[key].value !== '') {
+            glutenFree = responses[key].value;
+        }
+        if (responses[key].name === 'vegetarian' && responses[key].value !== '') {
+            vegetarian = responses[key].value;
+        }
+        if (responses[key].name === 'ingredients' && responses[key].value !== '') {
+            ingredients = responses[key].value;
+        }
     }
 
-    const item = new Item(id, name, description, category);
+    const item = new Item(id, name, description, price, category, image, cuisine, vegan, vegetarian, glutenFree, ingredients);
 
-    $.post("/menu", {item}).then(function() {
+    $.post("/items", {item}).then(function() {
         window.location = '/items';
     })
 

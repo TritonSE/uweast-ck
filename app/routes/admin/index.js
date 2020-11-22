0e9e787b97e3bdb66f16ce3baa7f5ea335fd30df
Declan Sullivan <declan@declansullivan.com>
2020-11-22 02:42:40 -0800

    Added delete item functionality

--- app/routes/admin/index.js
+++ app/routes/admin/index.js
@@ -5,20 +5,34 @@ const router = express.Router();
 
 // Regular get, no params or extra routing.
 router.get('/', (req, res, next) => {
-    res.render('admin');
+    const items = [];
+    db.getAllMenuItems().then((allItems) => {
+        for (const key in allItems) {
+          const childData = allItems[key];
+          items.push(childData);
+        }
+        res.render('admin', { items });
+    }).catch((error) => {
+        log.error(error);
+    });
 });
 
 // Post data, log data to terminal.
 router.post('/addItem', (req, res, next) => {
     db.addNewItem(buildItemJSON(req.body));
-    res.render('admin');
+    res.redirect('/admin');
+});
+
+router.post('/deleteItem', (req, res, next) => {
+    db.deleteItem(req.body.id);
+    res.redirect('/admin');
 });
 
 function buildItemJSON(body) {
     body['vegan'] = (body['vegan'] === undefined) ? false : true;
     body['vegetarian'] = (body['vegetarian'] === undefined) ? false : true;
     body['glutenFree'] = (body['glutenFree'] === undefined) ? false : true;
-    body['ingredients'] = body['ingredients'].split(",");
+    body['ingredients'] = body['ingredients'].split(", ");
     body['price'] = parseFloat(body['price']);
 
     return body;
